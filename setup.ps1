da2fd78c7d208a7dc78b323e2de0ed5cd0bea621
Cayde01 <84088250+Cayde01@users.noreply.github.com>
2021-10-28 03:47:22 +0200

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,6 +1,20 @@
 Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled False
 & {$P = $env:TEMP + '\chromeremotedesktophost.msi'; Invoke-WebRequest 'https://dl.google.com/edgedl/chrome-remote-desktop/chromeremotedesktophost.msi' -OutFile $P; Start-Process $P -Wait; Remove-Item $P}
 
+start chrome
+Sleep 1
+$wshell = New-Object -ComObject wscript.shell; # shell for sending keys
+$wshell.AppActivate('Chrome') # make sure chrome is the active window
+Sleep 1
+$wshell.SendKeys('chrome://settings/help')
+Sleep .5
+$wshell.SendKeys("{ENTER}")
+Sleep 300 # sleep to allow updates to be processed 
+# change sleep values if computer is slow.
+$wshell.AppActivate('Chrome') # make sure chrome is the active window
+Sleep 1
+$wshell.SendKeys("%{F4}") # ALT F4
+
 New-Item -ItemType "directory" -Force -Path "c:\down"
 New-Item -ItemType "directory" -Force -Path "c:\rclone"
 New-Item -ItemType "directory" -Force -Path "c:\TOOLS2"
