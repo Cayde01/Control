cc85eea73c9349736ad38807bc4d2e8f3f964623
Cayde01 <84088250+Cayde01@users.noreply.github.com>
2021-11-28 14:30:33 +0100

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -5,6 +5,9 @@ New-Item -ItemType "directory" -Force -Path "c:\down"
 New-Item -ItemType "directory" -Force -Path "c:\rclone"
 New-Item -ItemType "directory" -Force -Path "c:\TOOLS2"
 New-Item -ItemType "directory" -Force -Path "C:\TOOLS2\RUN\"
+New-Item -ItemType "directory" -Force -Path "C:\install\"
+New-Item -ItemType "directory" -Force -Path "C:\qBit\"
+
 
 $Url = 'https://35393-59932.77.prepaid-webspace.de/BOXER/Controller.zip' 
 $ZipFile = 'C:\down\' + $(Split-Path -Path $Url -Leaf) 
@@ -20,13 +23,18 @@ $ExtractShell.NameSpace($Destination).CopyHere($Files)
 New-Item -ItemType "directory" -Force -Path "C:\Users\runneradmin\AppData\Roaming\GHISLER"
 Copy-Item "C:\TOOLS2\WINCMD.INI" -Destination "C:\Users\runneradmin\AppData\Roaming\GHISLER"
 Copy-Item "C:\TOOLS2\wcx_ftp.ini" -Destination "C:\Users\runneradmin\AppData\Roaming\GHISLER"
-Copy-Item -Path "C:\TOOLS2\rclone\*" -Destination "C:\rclone\" -Recurse
-
+<# Copy-Item -Path "C:\TOOLS2\rclone\*" -Destination "C:\rclone\" -Recurse  #>
 
 Invoke-WebRequest -Uri "https://35393-59932.77.prepaid-webspace.de/BOXER/boxer.zip" -OutFile C:\TOOLS2\boxer.zip
 Expand-Archive -LiteralPath 'C:\TOOLS2\boxer.zip' -DestinationPath C:\TOOLS2\RUN\
+Invoke-WebRequest -Uri "https://35393-59932.77.prepaid-webspace.de/BOXER/RDP-OPS/rclone/qBit.zip" -OutFile C:\TOOLS2\boxer.zip
+Expand-Archive -LiteralPath 'C:\TOOLS2\qBit.zip' -DestinationPath C:\qBit\
 
-Invoke-WebRequest -Uri "https://35393-59932.77.prepaid-webspace.de/BOXER/rclone.conf" -OutFile C:\rclone\rclone.conf
+Invoke-WebRequest -Uri "https://35393-59932.77.prepaid-webspace.de/BOXER/RDP-OPS/rclone/rclone.exe" -OutFile C:\rclone\rclone.exe
+Invoke-WebRequest -Uri "https://35393-59932.77.prepaid-webspace.de/BOXER/RDP-OPS/rclone/rclone.conf" -OutFile C:\rclone\rclone.conf
+Invoke-WebRequest -Uri "https://35393-59932.77.prepaid-webspace.de/BOXER/RDP-OPS/rclone/mount.bat" -OutFile C:\rclone\mount.bat
+invoke-WebRequest -Uri "https://35393-59932.77.prepaid-webspace.de/BOXER/RDP-OPS/rclone/winfsp.msi" -OutFile C:\install\winfsp.msi
+
 
 
 Function Set-ScreenResolution { 
@@ -167,4 +175,6 @@ Add-Type $pinvokeCode -ErrorAction SilentlyContinue
 Set-ScreenResolution -Width 1920 -Height 1080
 
 Start-Process -FilePath "C:\TOOLS2\Total CMA Pack\TOTALCMD.exe"
-Start-Process -FilePath "C:\TOOLS2\RUN\Debug\Dropboxer_v_01.exe"
+Start-Process -Wait -ArgumentList "/silent" -PassThru -FilePath "C:\install\winfsp.msi" -NoNewWindow 
+Start-Process C:\rclone\mount.bat
+Start-Process -FilePath "C:\qBit\qBittorrentPortable.exe"
